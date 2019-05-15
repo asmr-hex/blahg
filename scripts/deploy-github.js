3f6cca02d5ba1ca32cd481d791033e60b2f4bc72
connorwalsh <c@polygon.pizza>
2019-05-15 17:11:46 -0400

    idk

--- scripts/deploy-github.js
+++ scripts/deploy-github.js
@@ -4,12 +4,9 @@ const ghpages = require('gh-pages')
 ghpages.publish(
   'public',
   {
-    branch: 'master',
     repo: 'https://github.com/connorwalsh/blahg.git',
-    dest: 'public',
   },
   () => {
-    
     console.log('Deploy Complete!')
   }
 )
