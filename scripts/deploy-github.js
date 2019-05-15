f54cf67137a8533edc9b8c5bd0596c67baca7d44
connorwalsh <c@polygon.pizza>
2019-05-15 16:03:15 -0400

    deploy directory should be `static`

--- scripts/deploy-github.js
+++ scripts/deploy-github.js
@@ -2,7 +2,7 @@ const ghpages = require('gh-pages')
 
 // replace with your repo url
 ghpages.publish(
-  'public',
+  'static',
   {
     branch: 'master',
     repo: 'https://github.com/connorwalsh/blahg.git',
